5135fb3dc70138cbcdf27855c0f82df70c0e41b4
roelfken <roman.oe@uni-muenster.de>
2024-02-17 14:49:57 +0100

    add frontend
    The basis of a vue frontend application is added in here to display the results of the algorithms

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <AppOverview>
+  </AppOverview>
+</template>
+
+<script>
+import AppOverview  from './components/AppOverview.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppOverview
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/AppOverview.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class = "navbar">
+        <p class = "weatherprog">
+            WeatherProg
+        </p>
+        <img src="..\assets\earth2.svg" alt="Weather Icon" class = "weathericon">
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.navbar{
+    width: 100%; 
+    position: fixed; 
+    top: 0; 
+    left: 0; 
+    background: linear-gradient(to right, rgb(255, 255, 255) 0%, rgb(189, 142, 189) 33%, rgb(189, 142, 189) 33%, rgb(128, 0, 128) 100%); 
+    text-align: left; 
+    color: black;
+    display: flex;
+    
+}
+
+.weatherprog{
+    font-size: x-large;
+    font-weight: 600;
+    padding-left: 0.5em;
+}
+
+.weathericon{
+    width: 195px; /* oder jede andere Größe, die Sie bevorzugen */
+    height: 195px; /* oder jede andere Größe, die Sie bevorzugen */
+    margin-right: 8px;
+    margin-top: -55px;
+    margin-bottom: -55px;
+    margin-left: -20px;
+}
+
+</style>
