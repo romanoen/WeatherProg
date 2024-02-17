203460ea6c6c65b2972b05a99afbec79a6276ea3
roelfken <roman.oe@uni-muenster.de>
2024-02-17 22:00:07 +0100

    added vuetify
    vuetify has been added to the project to increase usability

--- frontend/src/components/AppOverview.vue
+++ frontend/src/components/AppOverview.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class = "navbar">
-        <p class = "weatherprog">
-            WeatherProg
-        </p>
-        <img src="..\assets\earth2.svg" alt="Weather Icon" class = "weathericon">
+    <div class="overview">
+        <div class="navbar">
+            <p class="weatherprog">
+                WeatherProg
+            </p>
+            <img src="/earth2.svg" alt="Weather Icon" class="weathericon">
+        </div>
+        <div class="utilities">
+            <div class="searchbar">
+                <v-text-field label="Search Location" placeholder="Münster" variant = "solo" rounded ></v-text-field>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -11,31 +19,48 @@
 </script>
 
 <style>
-.navbar{
-    width: 100%; 
-    position: fixed; 
-    top: 0; 
-    left: 0; 
-    background: linear-gradient(to right, rgb(255, 255, 255) 0%, rgb(189, 142, 189) 33%, rgb(189, 142, 189) 33%, rgb(128, 0, 128) 100%); 
-    text-align: left; 
+.navbar {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: linear-gradient(to right, rgb(255, 255, 255) 0%, rgb(189, 142, 189) 33%, rgb(189, 142, 189) 33%, rgb(128, 0, 128) 100%);
+    text-align: left;
     color: black;
     display: flex;
-    
+
 }
 
-.weatherprog{
+.weatherprog {
     font-size: x-large;
     font-weight: 600;
     padding-left: 0.5em;
+    margin-top: 1em;
 }
 
-.weathericon{
-    width: 195px; /* oder jede andere Größe, die Sie bevorzugen */
-    height: 195px; /* oder jede andere Größe, die Sie bevorzugen */
+.weathericon {
+    width: 195px;
+    /* oder jede andere Größe, die Sie bevorzugen */
+    height: 195px;
+    /* oder jede andere Größe, die Sie bevorzugen */
     margin-right: 8px;
     margin-top: -55px;
     margin-bottom: -55px;
     margin-left: -20px;
 }
 
+.overview {
+    display: flex;
+    flex-direction: column;
+}
+
+.utilities {
+    display: flex;
+    justify-content: center;
+    margin-top: 2em;
+}
+
+.searchbar {
+    width: 40em;
+}
 </style>
